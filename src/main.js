1ab1100cd3ee71fdadd987d393c756675e99c155
CatalinaJL <catalina.p.jl@gmail.com>
2020-07-02 12:20:23 -0400

    prubas de console.log

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@
 */
 import data from './data/potter/potter.js';
 
-console.log(data[14]);
+/*console.log(data[0,1].house);
+*/
+console.table(data[3].wand); 
 
 document.getElementById("Personajes").innerHTML = "¡Bienvenido!" + "estos son los personajes" +":" + data[0].name, data[1].name;
 
